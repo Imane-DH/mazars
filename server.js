16a63b835fd87d0d5a0afbdb2ebd4d063148561a
Imane-DH <dahaniimane9@gmail.com>
2021-09-17 14:05:30 +0200

    make it better

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
 
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
+const indexRouter = require("./routes/index");
 
-const indexRouter = require('./routes/index')
+app.set("view engine", "ejs"),
+  app.set("views", __dirname + "/views"),
+  app.set("layout", "layouts/layout"),
+  app.use(expressLayouts),
+  app.use(express.static("public"));
 
-app.set('view engine', 'ejs'),
-app.set('views', __dirname + '/views'),
-app.set('layout', 'layouts/layout'),
-app.use(expressLayouts),
-app.use(express.static('public'));
+const mongoose = require("mongoose");
 
-
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
 db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("connecté à Mongoose")
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("connecté à Mongoose");
 });
 
-app.use('/', indexRouter)
+app.use("/", indexRouter);
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
